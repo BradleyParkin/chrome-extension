75f94d33783d001ebbdfa82323fbb61b818af0a1
BradleyParkin <bradbusiness@hotmail.com>
2022-12-23 15:07:29 +0000

    add inner.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let myLeads = []
+let myLeads = ["www.test1.com", "www.test1.com", "www.test1.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[1] + "</li>"
+     
+}
