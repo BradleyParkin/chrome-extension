605ee56190e25cb5af38bf2497c24fe4ec78291c
BradleyParkin <bradbusiness@hotmail.com>
2022-12-23 18:18:20 +0000

    finished for now

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
+    console.log( localStorage.getItem("myLeads") )
 })
 
 
@@ -18,7 +25,8 @@ function renderLeads() {
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
-            </li>`
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
